f9c28f1e6cbc0a4637bd8a846725e9308d4caa95
Captainjack-kor <jennystr233@gmail.com>
2022-05-24 11:06:44 +0900

    backup

--- NetworkManager.cs
+++ NetworkManager.cs
@@ -16,3 +16,54 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     
     public override void OnJoinedRoom() { }
 }
+
+/*
+
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+  public Text StatusText;
+  public InputField NickNameInput;
+  public GameObject Cube;
+  public string gameVersion = "1.0";
+
+
+  void Awake()
+  {
+      PhotonNetwork.AutomaticallySyncScene = true;
+  }
+
+  void Start()
+  {
+      PhotonNetwork.GameVersion = this.gameVersion;
+      PhotonNetwork.ConnectUsingSettings();
+  }
+
+  void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+  override void OnConnectedToMaster()
+  {
+      PhotonNetwork.JoinRandomRoom();
+  }
+
+  override void OnJoinedRoom()
+  {
+      PhotonNetwork.Instantiate("Player3", Cube.transform.position, Quaternion.identity);
+  }
+
+  override void OnJoinRandomFailed(short returnCode, string message)
+  {
+      this.CreateRoom();
+  }
+
+  void CreateRoom()
+  {
+      PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+  }
+}
+
+*/
